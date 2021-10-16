af139213f281f6fa79161e492c64ba6613b9dc7c
mcorries <90268233+mcorries@users.noreply.github.com>
2021-10-16 14:48:54 +0200

    Update DesktopGame.java

--- src/main/java/com/mcnz/rps/DesktopGame.java
+++ src/main/java/com/mcnz/rps/DesktopGame.java
@@ -7,7 +7,7 @@ public class DesktopGame {
 
 	public static void main(String[] args) {
 
-		String prompt = "Will it be rock, paper or scissors?";
+		String prompt = "Welcome to RPS, Will it be rock, paper or scissors?";
 		Score score = new Score();
 		java.util.Vector<GameSummary> history = new java.util.Vector<GameSummary>();
 
