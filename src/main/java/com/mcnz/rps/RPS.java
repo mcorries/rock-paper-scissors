d19ca4e388028c17aa12cd783dcc6d010d8754f7
mcorries <90268233+mcorries@users.noreply.github.com>
2021-11-15 22:52:21 +0200

    Update RPS.java

--- src/main/java/com/mcnz/rps/RPS.java
+++ src/main/java/com/mcnz/rps/RPS.java
@@ -4,7 +4,7 @@ class RPS {
   public static void main(String[] args) {
     
 		//Initialize the Scanner and print a welcome message
-		Scanner in = new Scanner(System.in);		System.out.println("******** Welcome to Rock, Paper, Scissors! ********");
+		Scanner in = new Scanner(System.in);		System.out.println("******** Welcome to ROCK, PAPER, SCISSORS! ********");
 
 		//Use a while(true) loop and only break the loop if the user wants to quit
 		while(true) {
